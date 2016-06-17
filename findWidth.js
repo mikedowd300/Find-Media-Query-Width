50c57e6aa05ea2d95884cb096778f7e5c982637a
mikedowd300 <mikedowd1972@gmail.com>
2016-06-17 11:51:10 -0600

    added formatting, ans now shows width the entire window and its parent element

--- findWidth.js
+++ findWidth.js
@@ -1,10 +1,14 @@
 $(window).resize(displayWidth); 
 
 function displayWidth() {
-	var myTag = $( '#window-width' ).closest();
+	$( '#window-width' ).css('visibility', 'visible');
 	var myWidth = $( window ).width();
 	var elemWidth = $( '#window-width' ).parent().width();
 	var str = '<p>' + 'Window width = ' + myWidth + '</p>' + '<p>' + 'Element width = ' + elemWidth + '</p>';
-	
 	$('#window-width').html(str);
+	hideWidth();
+}
+
+function hideWidth() {
+    setTimeout(function(){ $( '#window-width' ).css('visibility', 'hidden'); }, 7000);
 }
